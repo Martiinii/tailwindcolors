2cb70ed1dffb85e3e0000527c0bdf50d1016861d
Martini <me.martiinii@gmail.com>
2022-04-30 22:27:46 +0200

    Added colors to safelist

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,12 @@ module.exports = {
   content: [
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
+  safelist: [
+    {
+      pattern: /(bg|text)-(slate|gray|zinc|neutral|stone|red|orange|amber|yellow|lime|green|emerald|teal|cyan|sky|blue|indigo|violet|purple|fuchsia|pink|rose)-(50|100|200|300|400|500|600|700|800|900)/,
+    },
+    'bg-red-400'
+  ],
   theme: {
     extend: {},
   },
